c72a7ea7dc545e4efee648ec6ad2c59ebcc7b326
michael-999 <michael.benison@hotmail.fr>
2020-11-13 23:17:49 +0100

    react.5

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,18 +10,12 @@ import './index.css';
 
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        
-        };
-    }
+   
     render() {
       return (
         <button className="square"
-         onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
+         onClick={() => this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
@@ -35,17 +29,24 @@ class Square extends React.Component {
           };
       }
 
+      handleClick(i) {
+          const squares = this.state.squares.slice();
+          squares[i] = 'X';
+          this.setState({squares: squares});
+      }
 
 
 
 
 
     renderSquare(i) {
-      return <Square value={this.state.sqares[i]} />;
+      return <Square
+       value={this.state.sqares[i]}
+       onClick={() => this.handleClick(i)} />;
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'LE PROCHAIN PLAYER: X';
   
       return (
         <div>
